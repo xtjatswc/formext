ed07379836183aa0297a8f82b5d32974744d3fc4
[qióng] <xtjatswc@users.noreply.github.com>
2018-01-30 14:23:50 +0800

    Autocomplete 支持单选、多选设置

--- form_rander/js/util.js
+++ form_rander/js/util.js
@@ -203,24 +203,29 @@ util.autocomplete = function(para){
         var params = eval("(" + para.requestPara.format(extractLast(request.term)) + ")");
 
         $.getJSON(para.url, {
-            sql : para.sql,
-            para: params
-        }, response);
+                sql : para.sql,
+                para: params
+            }, response);
         },
         focus: function() {
-        // prevent value inserted on focus
-        return false;
+            // prevent value inserted on focus
+            // 不让多选时，点选的值覆盖掉之前已选的值
+            return false;
         },
         select: function( event, ui ) {
-        var terms = split( this.value );
-        // remove the current input
-        terms.pop();
-        // add the selected item
-        terms.push( ui.item.value );
-        // add placeholder to get the comma-and-space at the end
-        terms.push( "" );
-        this.value = terms.join( ", " );
-        return false;
+            if(!para.multiple){
+                //单选
+                return true;
+            }
+            var terms = split( this.value );
+            // remove the current input
+            terms.pop();
+            // add the selected item
+            terms.push( ui.item.value );
+            // add placeholder to get the comma-and-space at the end
+            terms.push( "" );
+            this.value = terms.join( ", " );
+            return false;
         }
     });
 }
--- js/patientinfo.js
+++ js/patientinfo.js
@@ -3,6 +3,7 @@ $(function($){
         id : "where_PatientName",
         url : "form_rander/query.php",
         minLength : 2,
+        multiple : true,
         sql : "select CONCAT(PatientName, '  [ ', PatientNameFirstLetter, ' ]') label, PatientName value from patientbasicinfo where (PatientName like :term or PatientNameFirstLetter like :term2) limit 0, 30",
         requestPara : '{":term" : "%{0}%", ":term2" : "{0}%"}',
     };
