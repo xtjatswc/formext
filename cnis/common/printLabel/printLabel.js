3aa808fb52da42d0d27f6ab320346c2a5680decf
[qióng] <xtjatswc@users.noreply.github.com>
2018-02-26 14:33:49 +0800

    打印样式

--- cnis/common/printLabel/printLabel.js
+++ cnis/common/printLabel/printLabel.js
@@ -22,32 +22,18 @@ printLabel.print = function () {
 
 printLabel.printInit = function(){
     LODOP = getLodop();
-    //指定整体偏移量、可编辑区域大小，默认单位px，名称很重要，打印维护保存的时候作为ID，所以程序修改发布后，如果想让客户端暂存失效，就改一下这个名
-    LODOP.PRINT_INITA(0, 0, "180mm", "100mm", "标签打印");
-    //设置纸张大小，默认单位mm
-    LODOP.SET_PRINT_PAGESIZE(2, 900, 500, "");
-    //
-    LODOP.ADD_PRINT_HTM(-2, -1, 260, 39, "打印页面部分内容a");
-    //强制分页，但别把它放到页眉代码下面，好像那头互相干扰不起作用
-    LODOP.NewPage();
-    //用ADD_PRINT_TABLE输出表格，固定高度表格，表格内容超过高度会自动分页
-    LODOP.ADD_PRINT_TABLE(42, -6, "80mm", "20mm", document.getElementById("tblNutrientadvicedetail").outerHTML);
-    //表格<thead>标记表头，用如下配置，会显示在每一页
-    LODOP.SET_PRINT_STYLEA(0, "TableHeightScope", 1);
 
-    //设置页眉、页脚的代码放到最后，才会起作用，不知道是咱回事，这是重点
-    //显示总页码，默认为阿拉伯数字，format='ChineseNum'表示显示成汉字
-    //也有示例用这个显示页码
-    //LODOP.ADD_PRINT_TEXT(421,542,165,22,"右下脚的页号：第#页/共&页");
-    //LODOP.SET_PRINT_STYLEA(0,"ItemType",2);
-
-    LODOP.ADD_PRINT_HTM(1, 600, 300, 100, "总页号：<font color='#0000ff' format='ChineseNum'><span tdata='pageNO'>第##页</span>/<span tdata='pageCount'>共##页</span></font>");
-    LODOP.SET_PRINT_STYLEA(0, "ItemType", 1);
-    //位置锁定，右边距锁定
-    LODOP.SET_PRINT_STYLEA(0, "Horient", 1);
-
-    LODOP.ADD_PRINT_TEXT(15, -1, 260, 39, "打印页面部分内容");
-    LODOP.SET_PRINT_STYLEA(0, "ItemType", 1);
+    LODOP.PRINT_INITA(0,0,"90.01mm","50.01mm","标签打印");
+    LODOP.SET_PRINT_PAGESIZE(2,900,500,"");
+    var strStyle=  document.getElementById("cssPrint").outerHTML;//"<style> table,td,th {border-width: 1px;border-style: solid;border-collapse: collapse}</style>"
+    LODOP.ADD_PRINT_TABLE("11.59mm","1.01mm","85.01mm","31.33mm",strStyle + document.getElementById("tblNutrientadvicedetail").outerHTML);
+    LODOP.SET_PRINT_STYLEA(0,"Vorient",3);
+    LODOP.ADD_PRINT_HTM("1.01mm","1.01mm","85.01mm","10mm",document.getElementById("divBaseInfo").outerHTML);
+    LODOP.SET_PRINT_STYLEA(0,"ItemType",1);
+    LODOP.SET_PRINT_STYLEA(0,"LinkedItem",1);
+    LODOP.ADD_PRINT_TEXT("43.66mm","1.32mm","21.17mm","5.29mm","第#页/共&页");
+    LODOP.SET_PRINT_STYLEA(0,"ItemType",2);
+    LODOP.SET_PRINT_STYLEA(0,"LinkedItem",1);
 
     LODOP.SET_SHOW_MODE("LANDSCAPE_DEFROTATED",1);//横向时的正向显示
 
--- cnis/common/printLabel/test.php
+++ cnis/common/printLabel/test.php
@@ -14,38 +14,91 @@ $page->randerPage();
 
 //css样式
 function randerStylesheetCallBack(){
-    //echo "css";
+    $version = form_rander\page::$_pageCfg["version"];
+    ?>
+	<link id="cssPrint" rel="stylesheet" type="text/css" media="screen" href="printLabel.css?v=<?php echo $version ?>" />    
+    <?php
 }
 
 //javascript
 function randerJavascriptCallBack(){
+    $version = form_rander\page::$_pageCfg["version"];
     ?>
-	<script language="javascript" type="text/javascript" src="printLabel.js"></script>    
+	<script language="javascript" type="text/javascript" src="printLabel.js?v=<?php echo $version ?>"></script>    
     <?php
 }
 
 //body
 function randerBodyCallBack(){
 ?>
-<input type="button" value="打印设计" onclick="printLabel.printDesign()" />
-<input type="button" value="打印维护" onclick="printLabel.printSetup()" />
-<input type="button" value="打印预览" onclick="printLabel.preview()" />
-<input type="button" value="打印" onclick="printLabel.print()" />
-<table id="tblNutrientadvicedetail">
-<thead>
-<tr>
-<td>品名</td>
-<td>数量</td>
-<td>规格</td>
-<td>备注</td>
-</tr>
-</thead>
-<tr>
-<td>倍康素</td>
-<td>2</td>
-<td>250ml</td>
-<td></td>
-</tr>
+<div>
+    <input type="button" value="打印设计" onclick="printLabel.printDesign()" />
+    <input type="button" value="打印维护" onclick="printLabel.printSetup()" />
+    <input type="button" value="打印预览" onclick="printLabel.preview()" />
+    <input type="button" value="打印" onclick="printLabel.print()" />
+</div>
+
+<div> 
+        <div id="divBaseInfo" >
+        <div style="float:left">姓名：张三</div> &nbsp;
+        <div style="float:left">科室：肿瘤内科</div> &nbsp;
+        <div style="float:left">床号：a12536</div> &nbsp;
+        <div style="float:left">住院号：0001254521</div> &nbsp;
+</div>
+
+<table  id="tblNutrientadvicedetail" class="gridtable">
+    <thead>
+        <tr>
+        <td>品名2</td>
+        <td>数量</td>
+        <td>规格</td>
+        <td>备注</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td>倍康素</td>
+        <td>2</td>
+        <td>250ml</td>
+        <td></td>
+        </tr>
+        <tr>
+        <td>倍康素2</td>
+        <td>2</td>
+        <td>250ml</td>
+        <td></td>
+        </tr>
+        <tr>
+        <td>倍康素3</td>
+        <td>2</td>
+        <td>250ml</td>
+        <td></td>
+        </tr>
+        <tr>
+        <td>倍康素4</td>
+        <td>2</td>
+        <td>250ml</td>
+        <td></td>
+        </tr>
+        <tr>
+        <td>倍康素5</td>
+        <td>2</td>
+        <td>250ml</td>
+        <td></td>
+        </tr>
+        <tr>
+        <td>倍康素6</td>
+        <td>2</td>
+        <td>250ml</td>
+        <td></td>
+        </tr>
+        <tr>
+        <td>倍康素</td>
+        <td>2</td>
+        <td>250ml</td>
+        <td></td>
+        </tr>
+    </tbody>
 </table>
 
 <?php
