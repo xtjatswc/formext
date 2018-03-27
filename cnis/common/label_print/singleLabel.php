fe56c0917793108a983eba7293e363625dc28175
[qióng] <xtjatswc@users.noreply.github.com>
2018-03-27 15:26:26 +0800

    标签格式调整

--- cnis/common/label_print/singleLabel.php
+++ cnis/common/label_print/singleLabel.php
@@ -3,7 +3,7 @@ require "../../../autoload.php";
 
 $detailDBKeys = $_GET["detailDBKeys"];
 
-$sql = "select  d.HospitalizationNumber, e.PatientName, f.DepartmentName, g.Bed, h.SysCodeName PreparationMode,a.TakeOrder from nutrientadvicedetail a INNER JOIN nutrientadvice b on a.NutrientAdvice_DBKey = b.NutrientAdvice_DBKey
+$sql = "select  c.NutrientAdviceSummary_DBKey,d.HospitalizationNumber, e.PatientName, f.DepartmentName, g.Bed, h.SysCodeName PreparationMode,a.TakeOrder from nutrientadvicedetail a INNER JOIN nutrientadvice b on a.NutrientAdvice_DBKey = b.NutrientAdvice_DBKey
 inner JOIN nutrientadvicesummary c on b.NutrientAdviceSummary_DBKey = c.NutrientAdviceSummary_DBKey
 inner join patienthospitalizebasicinfo d on d.PatientHospitalize_DBKey = c.PatientHospitalize_DBKey
 inner join patientbasicinfo e on e.PATIENT_DBKEY = d.PATIENT_DBKEY
@@ -12,18 +12,27 @@ left join bednumber g on g.BedNumber_DBKey = d.BedNumber_DBKey
 left join syscode h on h.SysCode = a.PreparationMode and h.SystemCodeTypeName = 'PreparationMode'
 where a.NutrientAdviceDetail_DBKEY in ($detailDBKeys) limit 0,1";
 $result = $db->fetch_row($sql);
-$baseInfo = "姓名:".$result["PatientName"]."&nbsp;科室:".$result["DepartmentName"]."&nbsp;床号:".$result["Bed"]."&nbsp;住院号:".$result["HospitalizationNumber"]."&nbsp;制剂方式:".$result["PreparationMode"]."&nbsp;服用时间:".$result["TakeOrder"]."&nbsp;";
+$baseInfo = "姓名:<font style='font-size: 11pt;'>".$result["PatientName"]."</font>&nbsp;
+科室:<font style='font-size: 11pt;'>" . $result["DepartmentName"] . "</font>&nbsp;
+床号:<font style='font-size: 11pt;'>" . $result["Bed"] . "</font>&nbsp;
+住院号:<font style='font-size: 11pt;'>" . $result["HospitalizationNumber"] . "</font>&nbsp;
+医嘱单号:<font style='font-size: 11pt;'>" . $result["NutrientAdviceSummary_DBKey"] . "</font>&nbsp;
+制剂方式:<font style='font-size: 11pt;'>" . $result["PreparationMode"] . "</font>&nbsp;
+服用时间:<font style='font-size: 11pt;'>" . $result["TakeOrder"] . "</font>&nbsp;";
+
 
 //制剂数据
-$sql = "select b.RecipeAndProductName,a.AdviceAmount,a.NutrientAdviceDetailRemark from nutrientadvicedetail a 
+$sql = "select b.RecipeAndProductName,a.AdviceAmount,a.NutrientAdviceDetailRemark,c.MeasureUnitName,d.MeasureUnitName minUnitName, b.wrapperType from nutrientadvicedetail a
 INNER JOIN recipeandproduct b on a.RecipeAndProduct_DBKey = b.RecipeAndProduct_DBKey
+left join measureunit c on c.MeasureUnit_DBKey = b.MeasureUnit_DBKey
+left join measureunit d on d.MeasureUnit_DBKey = b.minUnit_DBKey
 where a.NutrientAdviceDetail_DBKEY in ($detailDBKeys)";
 $tblDetail = $db->fetch_all($sql);
 ?>
 
 <div class="labelContent">
-<div> 
-     <?php echo $baseInfo ?> 
+<div>
+     <?php echo $baseInfo ?>
 </div>
 
 <table  id="tblNutrientadvicedetail" style="width:100%">
@@ -36,14 +45,32 @@ $tblDetail = $db->fetch_all($sql);
     </thead>
     <tbody>
         <?php
-        foreach ($tblDetail as $key => $value) {
-            echo "<tr>
-            <td>".$value["RecipeAndProductName"]."</td>
-            <td>".$value["AdviceAmount"]."</td>
-            <td>".$value["NutrientAdviceDetailRemark"]."</td>
-            </tr>";   
-        }       
-        ?>
+foreach ($tblDetail as $key => $value) {
+    $unit = "";
+    if($value["wrapperType"] == "1"){
+        $unit = $value["minUnitName"];
+    }else{
+        $unit = $value["MeasureUnitName"];
+    }
+
+    echo "<tr>
+            <td>" . $value["RecipeAndProductName"] . "</td>
+            <td>" . $value["AdviceAmount"] . " ". $unit . "</td>
+            <td>" . $value["NutrientAdviceDetailRemark"] . "</td>
+            </tr>";
+}
+?>
     </tbody>
 </table>
+禁止静脉注入<br/>
+室常温保存不超过6小时，4℃保存不超过12小时<br/>
+<?php
+$PreparationMode = $result["PreparationMode"];
+if ($PreparationMode == "粉剂") {
+    echo "用法：温水冲服<br/>";
+} else if ($PreparationMode == "管饲") {
+    echo "输注速度：<br/>";
+}
+
+?>
 </div>
