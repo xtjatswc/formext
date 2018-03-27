bef0c0c377788865b1b0d259ddf1fc0cdace5781
[qióng] <xtjatswc@users.noreply.github.com>
2018-03-27 18:37:25 +0800

    1

--- cnis/common/label_print/singleLabel.php
+++ cnis/common/label_print/singleLabel.php
@@ -12,7 +12,7 @@ left join bednumber g on g.BedNumber_DBKey = d.BedNumber_DBKey
 left join syscode h on h.SysCode = a.PreparationMode and h.SystemCodeTypeName = 'PreparationMode'
 where a.NutrientAdviceDetail_DBKEY in ($detailDBKeys) limit 0,1";
 $result = $db->fetch_row($sql);
-$baseInfo = "姓名:<font style='font-size: 11pt;'>".$result["PatientName"]."</font>&nbsp;
+$baseInfo = "姓名:<font style='font-size: 11pt;'>" . $result["PatientName"] . "</font>&nbsp;
 科室:<font style='font-size: 11pt;'>" . $result["DepartmentName"] . "</font>&nbsp;
 床号:<font style='font-size: 11pt;'>" . $result["Bed"] . "</font>&nbsp;
 住院号:<font style='font-size: 11pt;'>" . $result["HospitalizationNumber"] . "</font>&nbsp;
@@ -20,48 +20,80 @@ $baseInfo = "姓名:<font style='font-size: 11pt;'>".$result["PatientName"]."</f
 制剂方式:<font style='font-size: 11pt;'>" . $result["PreparationMode"] . "</font>&nbsp;
 服用时间:<font style='font-size: 11pt;'>" . $result["TakeOrder"] . "</font>&nbsp;";
 
-
 //制剂数据
-$sql = "select b.RecipeAndProductName,a.AdviceAmount,a.NutrientAdviceDetailRemark,c.MeasureUnitName,d.MeasureUnitName minUnitName, b.wrapperType from nutrientadvicedetail a
+$sql = "select b.RecipeAndProductName,a.AdviceAmount,a.NutrientAdviceDetailRemark,c.MeasureUnitName, b.wrapperType from nutrientadvicedetail a
 INNER JOIN recipeandproduct b on a.RecipeAndProduct_DBKey = b.RecipeAndProduct_DBKey
 left join measureunit c on c.MeasureUnit_DBKey = b.MeasureUnit_DBKey
-left join measureunit d on d.MeasureUnit_DBKey = b.minUnit_DBKey
 where a.NutrientAdviceDetail_DBKEY in ($detailDBKeys)";
 $tblDetail = $db->fetch_all($sql);
 ?>
 
 <div class="labelContent">
-<div>
-     <?php echo $baseInfo ?>
-</div>
 
-<table  id="tblNutrientadvicedetail" style="width:100%">
+<table class="table_bottom_none">
+    <tr>
+    <td>姓名：<?php echo $result["PatientName"] ?></td>
+    <td>科室：<?php echo $result["DepartmentName"] ?></td>
+    <td>床号：<?php echo $result["Bed"] ?></td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+    <td style="border-right:none;border-top:none;">品名</td>
+    <td style="border-left:none;border-top:none;">
+        <table>
+            <tr>
+                <td style="border:none;">
+                <table  id="tblNutrientadvicedetail">
     <thead>
         <tr>
-        <td>品名</td>
-        <td><nobr>数量</nobr></td>
-        <td>备注</td>
+        <td style="border-top:none;">品名</td>
+        <td style="border-top:none;"><nobr>数量</nobr></td>
+        <td style="border-right:none;border-top:none;">备注</td>
         </tr>
     </thead>
     <tbody>
         <?php
 foreach ($tblDetail as $key => $value) {
     $unit = "";
-    if($value["wrapperType"] == "1"){
-        $unit = $value["minUnitName"];
-    }else{
+    if ($value["wrapperType"] == "1") {
+        $unit = $value["MeasureUnitName"];
+    } else {
         $unit = $value["MeasureUnitName"];
     }
 
     echo "<tr>
             <td>" . $value["RecipeAndProductName"] . "</td>
-            <td>" . $value["AdviceAmount"] . " ". $unit . "</td>
-            <td>" . $value["NutrientAdviceDetailRemark"] . "</td>
+            <td>" . $value["AdviceAmount"] . " " . $unit . "</td>
+            <td style='border-right:none;'>" . $value["NutrientAdviceDetailRemark"] . "</td>
             </tr>";
 }
 ?>
     </tbody>
 </table>
+                </td>
+            </tr>
+            <tr>
+                <td style="border:none;">
+                    <table>
+                        <tr>
+                        <td style="border-bottom:none;border-top:none;">2</td>
+                        <td style="border-bottom:none;border-right:none;border-top:none;">3</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </td>
+    </tr>
+</table>
+
+<div>
+     <?php echo $baseInfo ?>
+</div>
+
+
 禁止静脉注入<br/>
 室常温保存不超过6小时，4℃保存不超过12小时<br/>
 <?php
