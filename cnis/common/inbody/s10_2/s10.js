2148603598a984dbdc135b2b8e4bc1d4044756a7
[qióng] <xtjatswc@users.noreply.github.com>
2018-06-13 10:14:15 +0800

    1

--- cnis/common/inbody/s10_2/s10.js
+++ cnis/common/inbody/s10_2/s10.js
@@ -1,7 +1,7 @@
-var inbody770 = {};
-inbody770.urlParams = util.urlToObject(window.location.search);
-inbody770.patient = null;
-inbody770.report = null;
+var s10 = {};
+s10.urlParams = util.urlToObject(window.location.search);
+s10.patient = null;
+s10.report = null;
 
 $(function ($) {
 
@@ -14,7 +14,8 @@ $(function ($) {
         }
 
         var timer1 = window.setInterval(function () {
-            if(inbody770.patient && inbody770.report){
+            if(s10.patient && s10.report){
+
                 window.clearInterval(timer1);
                 if ($("#printerName").html() == "#未设置#") {
                     if (confirm("未设置打印机，是否输出到默认打印机？")) {
@@ -28,54 +29,51 @@ $(function ($) {
 
     });
 
-    if(!inbody770.urlParams || typeof(inbody770.urlParams.reportId) == "undefined"){
+    if(!s10.urlParams || typeof(s10.urlParams.reportId) == "undefined"){
         alert("未获取到报告ID！");
         return;
     }
 
-    var sql = "select a.*,b.Height,b.HospitalizationNumber,date_format(c.DateOfBirth,'%Y-%m-%d') DateOfBirth from inbodyreport a inner join patienthospitalizebasicinfo b on a.PatientHospitalize_DBKey = b.PatientHospitalize_DBKey inner join patientbasicinfo c on c.PATIENT_DBKEY = b.PATIENT_DBKEY where  a.InBodyReport_DBKey = " + inbody770.urlParams.reportId + ";";
+    var sql = "select a.*,b.Height,b.HospitalizationNumber,date_format(c.DateOfBirth,'%Y-%m-%d') DateOfBirth from inbodyreport a inner join patienthospitalizebasicinfo b on a.PatientHospitalize_DBKey = b.PatientHospitalize_DBKey inner join patientbasicinfo c on c.PATIENT_DBKEY = b.PATIENT_DBKEY where  a.InBodyReport_DBKey = " + s10.urlParams.reportId + ";";
     $.getJSON(pageExt.libPath + "query.php", { sql: sql }, function (data, status, xhr) {
-        inbody770.patient = data[0];
-        if(inbody770.patient.InbodyModel == "s10"){
-            location.href = "../../../route.php?opt=inbody/s10_2/s10&reportId=" + inbody770.urlParams.reportId;
-        }        
+        s10.patient = data[0];
     });
-    
-    sql = "select a.*,b.ItemName from inbodyresult a inner join inbodyitem b on a.ItemCode = b.ItemCode and b.InbodyModel = '770' where InBodyReport_DBKey = " + inbody770.urlParams.reportId + ";";
+
+    sql = "select a.*,b.ItemName from inbodyresult a inner join inbodyitem b on a.ItemCode = b.ItemCode and b.InbodyModel = 's10' where InBodyReport_DBKey = " + s10.urlParams.reportId + ";";
     $.getJSON(pageExt.libPath + "query.php", { sql: sql }, function (data, status, xhr) {
-        inbody770.report = data;
+        s10.report = data;
     });
 
 
 });
     
 
-inbody770.printSetting = function () {
+s10.printSetting = function () {
     window.open(pageExt.libPath + "printerSet.php");
 }
 
-inbody770.printDesign = function () {
-    inbody770.printLoad(1);
+s10.printDesign = function () {
+    s10.printLoad(1);
 }
 
-inbody770.printSetup = function () {
-    inbody770.printLoad(2);
+s10.printSetup = function () {
+    s10.printLoad(2);
 }
 
-inbody770.preview = function () {
-    inbody770.printLoad(3);
+s10.preview = function () {
+    s10.printLoad(3);
 }
 
-inbody770.print = function () {
+s10.print = function () {
 
-    inbody770.printLoad(4);
+    s10.printLoad(4);
     // alert("请等待打印完毕后，再关闭该页面！");
 }
 
-inbody770.printLoad = function (flag) {
+s10.printLoad = function (flag) {
     // LODOP = getLodop();
 
-    inbody770.createPrintPage();
+    s10.createPrintPage();
     if (flag == 1) {
         LODOP.PRINT_DESIGN();
         return false;
@@ -90,9 +88,9 @@ inbody770.printLoad = function (flag) {
     }
 }
 
-inbody770.createPrintPage = function () {
+s10.createPrintPage = function () {
 
-    LODOP.PRINT_INITA(0, 0, "210mm", "297mm", "InBody770报告纸打印");
+    LODOP.PRINT_INITA(0, 0, "210mm", "297mm", "InBody s10报告纸打印");
     if (util.printerSetting.PrinterName == "#未设置#") {
         $("#lsMsg").html("尚未设置默认的人体成分报告打印机！");
     } else {
@@ -119,94 +117,94 @@ inbody770.createPrintPage = function () {
 
 
 
-    LODOP.ADD_PRINT_TEXT(100,64,100,20,inbody770.patient.HospitalizationNumber); //ID
-    LODOP.ADD_PRINT_TEXT(119,64,100,20,"(" + inbody770.patient.PatientName + ")"); //姓名
-    LODOP.ADD_PRINT_TEXT(110,193,100,20,inbody770.report[4].ItemValue + "cm"); //身高
-    LODOP.ADD_PRINT_TEXT(107,269,100,20,inbody770.report[5].ItemValue); //年龄
-    LODOP.ADD_PRINT_TEXT(122,268,100,20,"(" + inbody770.patient.DateOfBirth + ")"); //生日
-    LODOP.ADD_PRINT_TEXT(108,337,100,20,inbody770.patient.Gender == "M" ? "男" : "女"); //性别
-    LODOP.ADD_PRINT_TEXT(112,374,166,20,inbody770.patient.TestTime); //测试时间
+    LODOP.ADD_PRINT_TEXT(100,64,100,20,s10.patient.HospitalizationNumber); //ID
+    LODOP.ADD_PRINT_TEXT(119,64,100,20,"(" + s10.patient.PatientName + ")"); //姓名
+    LODOP.ADD_PRINT_TEXT(110,193,100,20,s10.report[4].ItemValue + "cm"); //身高
+    LODOP.ADD_PRINT_TEXT(107,269,100,20,s10.report[5].ItemValue); //年龄
+    LODOP.ADD_PRINT_TEXT(122,268,100,20,"(" + s10.patient.DateOfBirth + ")"); //生日
+    LODOP.ADD_PRINT_TEXT(108,337,100,20,s10.patient.Gender == "M" ? "男" : "女"); //性别
+    LODOP.ADD_PRINT_TEXT(112,374,166,20,s10.patient.TestTime); //测试时间
     LODOP.ADD_PRINT_HTML(35,526,254,75,$("#reportTitle")[0].outerHTML); //Title
-    LODOP.ADD_PRINT_TEXT(176,154,150,20,inbody770.report[19].ItemValue); //身体总水分 测量值
-    LODOP.ADD_PRINT_TEXT(189,130,150,20,"(" + inbody770.report[174].ItemValue + "~" + inbody770.report[175].ItemValue + ")"); //身体总水分 范围
-    LODOP.ADD_PRINT_TEXT(181,221,150,20,inbody770.report[19].ItemValue); //身体总水分
-    LODOP.ADD_PRINT_TEXT(203,154,150,20,inbody770.report[16].ItemValue); //蛋白质
-    LODOP.ADD_PRINT_TEXT(216,130,150,20,"(" + inbody770.report[149].ItemValue + "~" + inbody770.report[148].ItemValue + ")"); //蛋白质 范围
-    LODOP.ADD_PRINT_TEXT(229,154,150,20,inbody770.report[17].ItemValue); //无机盐
-    LODOP.ADD_PRINT_TEXT(242,130,150,20,"(" + inbody770.report[151].ItemValue + "~" + inbody770.report[150].ItemValue + ")"); //无机盐 范围
-    LODOP.ADD_PRINT_TEXT(255,154,150,20,inbody770.report[18].ItemValue); //体脂肪
-    LODOP.ADD_PRINT_TEXT(268,130,150,20,"(" + inbody770.report[170].ItemValue + "~" + inbody770.report[171].ItemValue + ")"); //体脂肪 范围
-    LODOP.ADD_PRINT_TEXT(189,300,150,20,inbody770.report[22].ItemValue); //肌肉量
-    LODOP.ADD_PRINT_TEXT(202,276,150,20,"(" + inbody770.report[178].ItemValue + "~" + inbody770.report[179].ItemValue + ")"); //肌肉量 范围
-    LODOP.ADD_PRINT_TEXT(209,375,150,20,inbody770.report[20].ItemValue); //去脂体重
-    LODOP.ADD_PRINT_TEXT(222,351,150,20,"(" + inbody770.report[176].ItemValue + "~" + inbody770.report[177].ItemValue + ")"); //去脂体重 范围
-    LODOP.ADD_PRINT_TEXT(229,446,150,20,inbody770.report[6].ItemValue); //体重
-    LODOP.ADD_PRINT_TEXT(242,422,150,20,"(" + inbody770.report[155].ItemValue + "~" + inbody770.report[154].ItemValue + ")"); //体重 范围
+    LODOP.ADD_PRINT_TEXT(176,154,150,20,s10.report[19].ItemValue); //身体总水分 测量值
+    LODOP.ADD_PRINT_TEXT(189,130,150,20,"(" + s10.report[174].ItemValue + "~" + s10.report[175].ItemValue + ")"); //身体总水分 范围
+    LODOP.ADD_PRINT_TEXT(181,221,150,20,s10.report[19].ItemValue); //身体总水分
+    LODOP.ADD_PRINT_TEXT(203,154,150,20,s10.report[16].ItemValue); //蛋白质
+    LODOP.ADD_PRINT_TEXT(216,130,150,20,"(" + s10.report[149].ItemValue + "~" + s10.report[148].ItemValue + ")"); //蛋白质 范围
+    LODOP.ADD_PRINT_TEXT(229,154,150,20,s10.report[17].ItemValue); //无机盐
+    LODOP.ADD_PRINT_TEXT(242,130,150,20,"(" + s10.report[151].ItemValue + "~" + s10.report[150].ItemValue + ")"); //无机盐 范围
+    LODOP.ADD_PRINT_TEXT(255,154,150,20,s10.report[18].ItemValue); //体脂肪
+    LODOP.ADD_PRINT_TEXT(268,130,150,20,"(" + s10.report[170].ItemValue + "~" + s10.report[171].ItemValue + ")"); //体脂肪 范围
+    LODOP.ADD_PRINT_TEXT(189,300,150,20,s10.report[22].ItemValue); //肌肉量
+    LODOP.ADD_PRINT_TEXT(202,276,150,20,"(" + s10.report[178].ItemValue + "~" + s10.report[179].ItemValue + ")"); //肌肉量 范围
+    LODOP.ADD_PRINT_TEXT(209,375,150,20,s10.report[20].ItemValue); //去脂体重
+    LODOP.ADD_PRINT_TEXT(222,351,150,20,"(" + s10.report[176].ItemValue + "~" + s10.report[177].ItemValue + ")"); //去脂体重 范围
+    LODOP.ADD_PRINT_TEXT(229,446,150,20,s10.report[6].ItemValue); //体重
+    LODOP.ADD_PRINT_TEXT(242,422,150,20,"(" + s10.report[155].ItemValue + "~" + s10.report[154].ItemValue + ")"); //体重 范围
 
     var leftMargin = 145;
     //肌肉脂肪分析
-    var w = inbody770.rangeWidth2(67, 85, 115);
+    var w = s10.rangeWidth2(67, 85, 115);
     LODOP.ADD_PRINT_SHAPE(4,351,132,w,9,0,1,"#808080"); //体重 %
-    LODOP.ADD_PRINT_TEXT(351,leftMargin + w,150,20,inbody770.report[6].ItemValue); //体重 kg    
+    LODOP.ADD_PRINT_TEXT(351,leftMargin + w,150,20,s10.report[6].ItemValue); //体重 kg    
 
-    w = inbody770.rangeWidth2(62, 90, 110);
+    w = s10.rangeWidth2(62, 90, 110);
     LODOP.ADD_PRINT_SHAPE(4,383,132,w,9,0,1,"#808080"); //骨骼肌 %
-    LODOP.ADD_PRINT_TEXT(383,leftMargin + w,150,20,inbody770.report[21].ItemValue); //骨骼肌 kg    
+    LODOP.ADD_PRINT_TEXT(383,leftMargin + w,150,20,s10.report[21].ItemValue); //骨骼肌 kg    
 
-    w = inbody770.rangeWidth2(61, 80, 160);
+    w = s10.rangeWidth2(61, 80, 160);
     LODOP.ADD_PRINT_SHAPE(4,414,132,w,9,0,1,"#808080"); //体脂肪 %
-    LODOP.ADD_PRINT_TEXT(414,leftMargin + w,150,20,inbody770.report[18].ItemValue); //体脂肪 kg    
+    LODOP.ADD_PRINT_TEXT(414,leftMargin + w,150,20,s10.report[18].ItemValue); //体脂肪 kg    
 
     //肥胖分析
-    w = inbody770.rangeWidth2(23, 18.5, 23.9);
+    w = s10.rangeWidth2(23, 18.5, 23.9);
     LODOP.ADD_PRINT_SHAPE(4,498,132,w,9,0,1,"#808080"); //身体质量指数
-    LODOP.ADD_PRINT_TEXT(498,leftMargin + w,150,20,inbody770.report[23].ItemValue); //身体质量指数 值
+    LODOP.ADD_PRINT_TEXT(498,leftMargin + w,150,20,s10.report[23].ItemValue); //身体质量指数 值
 
-    w = inbody770.rangeWidth2(24, 10, 20);
+    w = s10.rangeWidth2(24, 10, 20);
     LODOP.ADD_PRINT_SHAPE(4,529,132,w,9,0,1,"#808080"); //体脂百分比
-    LODOP.ADD_PRINT_TEXT(529,leftMargin + w,150,20,inbody770.report[24].ItemValue); //体脂百分比 值
+    LODOP.ADD_PRINT_TEXT(529,leftMargin + w,150,20,s10.report[24].ItemValue); //体脂百分比 值
 
     //肌肉均衡
-    w = inbody770.rangeWidth2(73, 85, 115);
+    w = s10.rangeWidth2(73, 85, 115);
     LODOP.ADD_PRINT_SHAPE(4,618,132,w,9,0,1,"#808080"); //右上肢 %
-    LODOP.ADD_PRINT_TEXT(618,leftMargin + w,150,20,inbody770.report[73].ItemValue); //右上肢 值
-    LODOP.ADD_PRINT_TEXT(618,457,150,20,inbody770.report[98].ItemValue); //细胞外水分比率 右上肢
+    LODOP.ADD_PRINT_TEXT(618,leftMargin + w,150,20,s10.report[73].ItemValue); //右上肢 值
+    LODOP.ADD_PRINT_TEXT(618,457,150,20,s10.report[98].ItemValue); //细胞外水分比率 右上肢
 
-    w = inbody770.rangeWidth2(74, 85, 115);
+    w = s10.rangeWidth2(74, 85, 115);
     LODOP.ADD_PRINT_SHAPE(4,653,132,w,9,0,1,"#808080"); //左上肢 %
-    LODOP.ADD_PRINT_TEXT(653,leftMargin + w,150,20,inbody770.report[74].ItemValue); //左上肢 值
-    LODOP.ADD_PRINT_TEXT(653,457,150,20,inbody770.report[99].ItemValue); //细胞外水分比率 左上肢
+    LODOP.ADD_PRINT_TEXT(653,leftMargin + w,150,20,s10.report[74].ItemValue); //左上肢 值
+    LODOP.ADD_PRINT_TEXT(653,457,150,20,s10.report[99].ItemValue); //细胞外水分比率 左上肢
 
-    w = inbody770.rangeWidth2(75, 90, 110);
+    w = s10.rangeWidth2(75, 90, 110);
     LODOP.ADD_PRINT_SHAPE(4,690,132,w,9,0,1,"#808080"); //躯干 %
-    LODOP.ADD_PRINT_TEXT(690,leftMargin + w,150,20,inbody770.report[75].ItemValue); //躯干 值
-    LODOP.ADD_PRINT_TEXT(690,457,150,20,inbody770.report[100].ItemValue); //细胞外水分比率 躯干
+    LODOP.ADD_PRINT_TEXT(690,leftMargin + w,150,20,s10.report[75].ItemValue); //躯干 值
+    LODOP.ADD_PRINT_TEXT(690,457,150,20,s10.report[100].ItemValue); //细胞外水分比率 躯干
 
-    w = inbody770.rangeWidth2(76, 90, 110);
+    w = s10.rangeWidth2(76, 90, 110);
     LODOP.ADD_PRINT_SHAPE(4,725,132,w,9,0,1,"#808080"); //右下肢 %
-    LODOP.ADD_PRINT_TEXT(725,leftMargin + w,150,20,inbody770.report[76].ItemValue); //右下肢 值
-    LODOP.ADD_PRINT_TEXT(725,457,150,20,inbody770.report[101].ItemValue); //细胞外水分比率 右下肢
+    LODOP.ADD_PRINT_TEXT(725,leftMargin + w,150,20,s10.report[76].ItemValue); //右下肢 值
+    LODOP.ADD_PRINT_TEXT(725,457,150,20,s10.report[101].ItemValue); //细胞外水分比率 右下肢
 
-    w = inbody770.rangeWidth2(77, 90, 110);
+    w = s10.rangeWidth2(77, 90, 110);
     LODOP.ADD_PRINT_SHAPE(4,761,132,w,9,0,1,"#808080"); //左下肢 %
-    LODOP.ADD_PRINT_TEXT(761,leftMargin + w,150,20,inbody770.report[77].ItemValue); //左下肢 值
-    LODOP.ADD_PRINT_TEXT(761,457,150,20,inbody770.report[102].ItemValue); //细胞外水分比率 左下肢
+    LODOP.ADD_PRINT_TEXT(761,leftMargin + w,150,20,s10.report[77].ItemValue); //左下肢 值
+    LODOP.ADD_PRINT_TEXT(761,457,150,20,s10.report[102].ItemValue); //细胞外水分比率 左下肢
 
     //细胞外水分比率分析
-    w = inbody770.rangeWidth2(103, 0.36, 0.39);
+    w = s10.rangeWidth2(103, 0.36, 0.39);
     LODOP.ADD_PRINT_SHAPE(4,849,132,w,9,0,1,"#808080"); //细胞外水分比率分析 线
-    LODOP.ADD_PRINT_TEXT(849,leftMargin + w,150,20,inbody770.report[103].ItemValue); //细胞外水分比率分析 值
+    LODOP.ADD_PRINT_TEXT(849,leftMargin + w,150,20,s10.report[103].ItemValue); //细胞外水分比率分析 值
 
     //历史折线图
-    inbody770.loadChart();
+    s10.loadChart();
 
     //右边栏
-    LODOP.ADD_PRINT_TEXT(197,585,100,20,inbody770.patient.HealthScore); //分值
+    LODOP.ADD_PRINT_TEXT(197,585,100,20,s10.patient.HealthScore); //分值
     LODOP.SET_PRINT_STYLEA(0,"FontSize",18.5);
 
     //内脏脂肪面积
-    var vfa = inbody770.report[30].ItemValue;
-    var age = inbody770.report[5].ItemValue;
+    var vfa = s10.report[30].ItemValue;
+    var age = s10.report[5].ItemValue;
     var xZero = 546;
     var x100 = 721;
     var yZero = 386;
@@ -217,8 +215,8 @@ inbody770.createPrintPage = function () {
     LODOP.SET_PRINT_STYLEA(0,"FontSize",18.5);
 
     //体型
-    var bmi = inbody770.report[23].ItemValue;
-    var PBF = inbody770.report[24].ItemValue;
+    var bmi = s10.report[23].ItemValue;
+    var PBF = s10.report[24].ItemValue;
     var x10 = 590;
     var x20 = 696;
     var y185 = 604;
@@ -231,43 +229,43 @@ inbody770.createPrintPage = function () {
     LODOP.SET_PRINT_STYLEA(0,"FontSize",23.5);
 
     //体重控制
-    LODOP.ADD_PRINT_TEXT(720,621,100,20,inbody770.report[204].ItemValue + " kg"); //目标体重
-    LODOP.ADD_PRINT_TEXT(740,621,100,20,inbody770.report[205].ItemValue + " kg"); //体重控制
-    LODOP.ADD_PRINT_TEXT(760,621,100,20,inbody770.report[206].ItemValue + " kg"); //脂肪控制
-    LODOP.ADD_PRINT_TEXT(780,621,100,20,inbody770.report[207].ItemValue + " kg"); //肌肉控制
+    LODOP.ADD_PRINT_TEXT(720,621,100,20,s10.report[204].ItemValue + " kg"); //目标体重
+    LODOP.ADD_PRINT_TEXT(740,621,100,20,s10.report[205].ItemValue + " kg"); //体重控制
+    LODOP.ADD_PRINT_TEXT(760,621,100,20,s10.report[206].ItemValue + " kg"); //脂肪控制
+    LODOP.ADD_PRINT_TEXT(780,621,100,20,s10.report[207].ItemValue + " kg"); //肌肉控制
 
     //研究项目
-    LODOP.ADD_PRINT_TEXT(819,621,100,20,inbody770.report[28].ItemValue + " kcal"); //基础代谢率
-    LODOP.ADD_PRINT_TEXT(839,621,142,20,inbody770.report[25].ItemValue + " （" + inbody770.report[163].ItemValue + "~" + inbody770.report[162].ItemValue + ")"); //腰臀比
-    LODOP.ADD_PRINT_TEXT(859,621,100,20,inbody770.report[30].ItemValue + " c㎡"); //内脏脂肪面积
-    var itemValue = inbody770.report[20].ItemValue / Math.pow(inbody770.report[4].ItemValue / 100,2)
+    LODOP.ADD_PRINT_TEXT(819,621,100,20,s10.report[28].ItemValue + " kcal"); //基础代谢率
+    LODOP.ADD_PRINT_TEXT(839,621,142,20,s10.report[25].ItemValue + " （" + s10.report[163].ItemValue + "~" + s10.report[162].ItemValue + ")"); //腰臀比
+    LODOP.ADD_PRINT_TEXT(859,621,100,20,s10.report[30].ItemValue + " c㎡"); //内脏脂肪面积
+    var itemValue = s10.report[20].ItemValue / Math.pow(s10.report[4].ItemValue / 100,2)
     LODOP.ADD_PRINT_TEXT(879,621,100,20,itemValue.toFixed(2) + " kg/㎡"); //去脂体重指数 去脂体重/身高平方
 
     //全身相位角
-    LODOP.ADD_PRINT_TEXT(914,647,100,20,inbody770.report[487].ItemValue + " °"); 
+    LODOP.ADD_PRINT_TEXT(914,647,100,20,s10.report[487].ItemValue + " °"); 
     LODOP.SET_PRINT_STYLEA(0,"FontSize",15.5);
 
-    var dzk = inbody770.loadDZk();
+    var dzk = s10.loadDZk();
     LODOP.ADD_PRINT_HTM("253mm","138mm","64mm","43mm", dzk);
 }
 
-inbody770.toFixed2 = function(index){
-    return parseFloat(inbody770.report[index].ItemValue).toFixed(2);
+s10.toFixed2 = function(index){
+    return parseFloat(s10.report[index].ItemValue).toFixed(2);
 }
 
-inbody770.range = function(min, max){
-    return parseFloat(inbody770.report[min].ItemValue).toFixed(2)  + "~" + parseFloat(inbody770.report[max].ItemValue).toFixed(2);
+s10.range = function(min, max){
+    return parseFloat(s10.report[min].ItemValue).toFixed(2)  + "~" + parseFloat(s10.report[max].ItemValue).toFixed(2);
 }
 
-inbody770.rangeWidth = function(value, min, max){
-    var s1 = inbody770.toFixed2(min);
-    var s2 =inbody770.toFixed2(max);
+s10.rangeWidth = function(value, min, max){
+    var s1 = s10.toFixed2(min);
+    var s2 =s10.toFixed2(max);
 
-    return inbody770.rangeWidth2(value, s1, s2);
+    return s10.rangeWidth2(value, s1, s2);
 }
 
-inbody770.rangeWidth2 = function(value, s1, s2){
-    var w = inbody770.toFixed2(value);
+s10.rangeWidth2 = function(value, s1, s2){
+    var w = s10.toFixed2(value);
 
     var dw = 83;//mm 低标准的范围宽度
     var ww = 62;//mm 标准值的范围宽度
@@ -277,10 +275,10 @@ inbody770.rangeWidth2 = function(value, s1, s2){
     return width;
 }
 
-inbody770.loadDZk = function(){
+s10.loadDZk = function(){
     var dzkObj = {};
     for(var i = 344; i <= 383; i++){
-        dzkObj[inbody770.report[i].ItemName] = inbody770.toFixed2(i);
+        dzkObj[s10.report[i].ItemName] = s10.toFixed2(i);
     }
     var style = $("#style1")[0].outerHTML;
     var dzk = $("#divDzk").html().format(dzkObj);
@@ -288,7 +286,7 @@ inbody770.loadDZk = function(){
 }
 
 //历史折线图
-;inbody770.loadChart = (function(){
+;s10.loadChart = (function(){
     var chart = {};
     chart.legendArr = [6, 21, 24, 103];
 
@@ -354,11 +352,6 @@ inbody770.loadDZk = function(){
             }
         }
 
-        if(max == min){
-            max++;
-            min--;
-        }
-
         return {max:max, min:min};
     }
 
@@ -387,7 +380,7 @@ inbody770.loadDZk = function(){
     chart.getData = function(){
         var d = null;
         $.ajaxSetup({async: false});
-        var sql = "select date_format(a.TestTime,'%y.%m.%d\r\n%H:%m') TestTime,b.* from inbodyreport a inner join inbodyresult b on a.InBodyReport_DBKey = b.InBodyReport_DBKey where a.InbodyModel = '770' and b.ItemCode in (6, 21, 24, 103) and a.PatientHospitalize_DBKey=" +  inbody770.patient.PatientHospitalize_DBKey + " order by a.TestTime desc, b.ItemCode limit 0,32;";
+        var sql = "select date_format(a.TestTime,'%y.%m.%d\r\n%H:%m') TestTime,b.* from inbodyreport a inner join inbodyresult b on a.InBodyReport_DBKey = b.InBodyReport_DBKey where a.InbodyModel = '770' and b.ItemCode in (6, 21, 24, 103) order by a.TestTime desc, b.ItemCode limit 0,32;";
         $.getJSON(pageExt.libPath + "query.php", { sql: sql }, function (data, status, xhr) {
             d = data;
         });
---
+++ cnis/common/inbody/s10_2/s10.php
@@ -0,0 +1,101 @@
+<?php
+require "../../../../autoload.php";
+
+form_rander\page::$_pageCfg = array(
+    'rootPath' => "..\\..\\..\\..\\",
+    'libPath' => "..\\..\\..\\..\\form_rander\\",
+    'Title' => "InBody s10报告纸打印",
+    'version' => $globalCfg["version"], //系统版本，变动时，js等缓存文件也会刷新
+    'debug' => $globalCfg["debug"],
+);
+
+$page = new form_rander\page($db);
+$page->randerPage();
+
+//css样式
+function randerStylesheetCallBack(){
+    ?>
+	<style id="style1">
+    table,td,th {border-width: 1px;border:1px solid black;border-collapse: collapse;font-size: 8pt;padding:1px;}
+    body{text-align: center;}
+    </style>
+    <?php
+}
+
+//javascript
+function randerJavascriptCallBack(){
+    $version = form_rander\page::$_pageCfg["version"];
+    ?>
+	<script language="javascript" type="text/javascript" src="s10.js?v=<?php echo $version ?>"></script>    
+    <?php
+}
+
+//body
+function randerBodyCallBack(){    
+    ?>
+    <div>
+        <input type="button" value="检测打印插件" onclick="util.CheckLodopIsInstall()" />
+        <input type="button" value="设置" onclick="s10.printSetting()" />
+        <input type="button" value="打印维护" onclick="s10.printSetup()" />
+        <input type="button" value="打印预览" onclick="s10.preview()" />
+        <input id="btnPrint" type="button" value="打印" onclick="s10.print()" />
+        <div style="display:none">
+            <input type="button" value="打印设计" onclick="s10.printDesign()" />
+        </div>
+    </div>
+    <br/>
+    打印机:      
+    <font id="printerName" color="blue"></font>&nbsp;&nbsp;<font id="lsMsg" color="red"></font>
+    <br/>
+    <font id="labelTip" color="red"></font>
+    <br/>    
+
+<div style="display:none">
+
+<div id="reportTitle" style="font-size:12pt;font-weight:bold;text-align: center;">
+重庆医科大学附属第三医院<br/>
+临床营养科<br/>
+人体成分测定报告<br/>
+</div>
+
+<?php
+$arr = array(
+    1 => "1Khz",
+    5 => "5Khz",
+    50 => "50Khz",
+    250 => "250KHz",
+    500 => "500KHz",
+    1000 => "1000KHz"
+);
+?>
+<div id="divDzk">
+<table>
+<tr>
+<th></th>
+<th>右上肢</th>
+<th>左上肢</th>
+<th>躯干</th>
+<th>右下肢</th>
+<th>左下肢</th>
+</tr>
+<?php
+foreach ($arr as $key => $value) {
+?>
+<tr>
+    <td><?php echo $value?></td>
+    <td>{<?php echo $key?>khz-RA Impedance}</td>
+    <td>{<?php echo $key?>khz-LA Impedance}</td>
+    <td>{<?php echo $key?>khz-TR Impedance}</td>
+    <td>{<?php echo $key?>khz-RL Impedance}</td>
+    <td>{<?php echo $key?>khz-LL Impedance}</td>
+</tr>
+<?php 
+}
+?>
+</table>
+</div>
+
+</div>
+
+    <?php
+}
